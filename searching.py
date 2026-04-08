61194119767e7a1ea0fc7cd59be5303ca83d8382
Adam Komarek <aduk0506@gmail.com>
2026-04-08 10:45:06 +0200

    update2_searching.py

--- searching.py
+++ searching.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import json
-
-
+from random import choices
+import time
+# import matplotlib.pyplot as plt
 def read_data(file_name, field):
 
     """
@@ -46,18 +47,42 @@ def linear_search(sequence, number):
 
 def binary_search(sequence, number):
     pozice = []
+
+    left = 0
+    right = len(sequence)-1
+    middle = (right - left)//2
     if number in sequence:
         for index, i in enumerate(sequence):
-            if number > sequence[len(sequence)/2]:
-
-            elif number < sequence[len(sequence)/2]:
-
+            if number == sequence[middle]:
+                return middle
+            elif number > sequence[middle]:
+                left += middle
 
             else:
-                return sequence[len(sequence)/2]
+                right -= middle
 
     else:
         return None
+# def unordered_sequence(max_len=100):
+#     return choices(range(-1000, 1000), k=max_len)
+#
+#
+# sizes = [100, 500, 1000, 5000, 10000]
+# times = [0.00001, 0.00003, 0.00006, 0.00031, 0.00067]
+#
+# plt.plot(sizes, times)
+#
+# plt.xlabel("Velikost vstupu")
+# plt.ylabel("Čas [s]")
+# plt.title("Ukázkový graf měření")
+# plt.show()
+
+def pattern_search(sekvence, vzor):
+    pozice = []
+
+
+    return pozice
+
 def main():
     vystup = read_data("sequential.json", "unordered_numbers")
     vyhledani = linear_search(vystup, 9)
